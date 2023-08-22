0fdda066c34e8f4fc9187524a757be16de8f44bc
kwa_laa <khoaddnguyen@gmail.com>
2023-08-22 15:57:48 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "./blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+# grab x & y coordinate for each state
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.tolist()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    # .title() accepts all lowercase/uppercase
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state name?").title()
+    print(answer_state)
+
+    # if answer_state is one of the states in the CSV file
+        # if correct
+            # create a turtle to write the state name that the x-y coor
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
